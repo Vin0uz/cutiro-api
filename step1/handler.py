a9af326b5a56ace6873b3b4e62c61f986a0ef07f
Kevin Vinhas <kevin@vinhas.fr>
2021-01-30 23:41:09 +0100

    Adapt from last version of step1 and step2 and destroy file to use the main one only.

--- step1/handler.py
+++ step1/handler.py
@@ -10,10 +10,10 @@ from utils import *
 def start(event, context):
   emis, payroll = read_data_from_event(event)
 
-  emis['teacher_name'] = clean_data(emis['teacher_name'])
-  emis['teacher_surname'] = clean_data(emis['teacher_surname'])
-  payroll['Surname'] = clean_data(payroll['Surname'])
-  payroll['First name'] = clean_data(payroll['First name'])
+  emis['clean_name'] = clean_data(emis['teacher_name'])
+  emis['clean_surname'] = clean_data(emis['teacher_surname'])
+  payroll['clean_surname'] = clean_data(payroll['Surname'])
+  payroll['clean_name'] = clean_data(payroll['First name'])
 
   step1, step2 = step_1_and_2(emis, payroll)
 
@@ -34,12 +34,12 @@ def mapping_perfect_match(emis, payroll):
   res = []
   for i in tqdm(payroll.index):
     p_id = payroll._get_value(i, 'Numéro Solde')
-    surname = payroll._get_value(i, 'Surname')
-    firstname = payroll._get_value(i, 'First name')
+    surname = payroll._get_value(i, 'clean_surname')
+    firstname = payroll._get_value(i, 'clean_name')
     dob = payroll._get_value(i, 'Date of birth')
     df = emis[(emis['teacher_surname'] == surname)&(emis['teacher_name'] == firstname)&(emis['Date of birth'] == dob)]
     for e_id in df['Numéro EMIS'].unique():
-      res.append((p_id, e_id))
+      res.append([p_id, e_id])
       j = df[df['Numéro EMIS'] == e_id].index
       try: df_emis = df_emis.drop(index = j, inplace = False)
       except: pass
@@ -72,12 +72,12 @@ def step_1_and_2(emis, payroll):
   for e in e_dict.keys():
     if len(e_dict[e]) == 1:
       p = e_dict[e][0]
-      if p_dict[p] == [e]: unique_mapping.append((e, p))
+      if p_dict[p] == [e]: unique_mapping.append([e, p])
     else:
       for p in e_dict[e]:
-        if (e,p) not in multiple_mapping: multiple_mapping.append((e, p))
+        if [e,p] not in multiple_mapping: multiple_mapping.append([e, p])
         for e_p in p_dict[p]:
-            if (e_p, p) not in multiple_mapping: multiple_mapping.append((e_p, p))
+            if [e_p, p] not in multiple_mapping: multiple_mapping.append([e_p, p])
 
 
   return unique_mapping, multiple_mapping
