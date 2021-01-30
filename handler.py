225c631a1cb737165c63195b1a3562e300c11e46
Kevin Vinhas <kevin@vinhas.fr>
2021-01-30 13:37:17 +0100

    Init serverless lambda python endpoint

---
+++ handler.py
@@ -0,0 +1,15 @@
+import json
+import datetime
+
+
+def endpoint(event, context):
+    body = {
+      "message": "Hello, World"
+    }
+
+    response = {
+      "statusCode": 200,
+      "body": json.dumps(body)
+    }
+
+    return response
