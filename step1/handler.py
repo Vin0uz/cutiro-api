82aaaf8446be1966319934fcb9f962a072597c5c
Kevin Vinhas <kevin@vinhas.fr>
2021-01-30 16:28:15 +0100

    Clean indentation from 4 spaces to 2 spaces

--- step1/handler.py
+++ step1/handler.py
@@ -36,23 +36,23 @@ def drop_punctuation(word):
 
 def mapping_perfect_match(emis, payroll):
   # This method returns a 1:1 matching list
-    df_emis = emis.copy()
-    df_payroll = payroll.copy()
-    res = []
-    for i in tqdm(payroll.index):
-        p_id = payroll._get_value(i, 'Numéro Solde')
-        surname = payroll._get_value(i, 'Surname')
-        firstname = payroll._get_value(i, 'First name')
-        dob = payroll._get_value(i, 'Date of birth')
-        df = emis[(emis['teacher_surname'] == surname)&(emis['teacher_name'] == firstname)&(emis['Date of birth'] == dob)]
-        for e_id in df['Numéro EMIS'].unique():
-            res.append((p_id, e_id))
-            j = df[df['Numéro EMIS'] == e_id].index
-            try: df_emis = df_emis.drop(index = j, inplace = False)
-            except: pass
-            try: df_payroll = df_payroll.drop(index = i, inplace = False)
-            except: pass
-    return res, df_emis, df_payroll
+  df_emis = emis.copy()
+  df_payroll = payroll.copy()
+  res = []
+  for i in tqdm(payroll.index):
+    p_id = payroll._get_value(i, 'Numéro Solde')
+    surname = payroll._get_value(i, 'Surname')
+    firstname = payroll._get_value(i, 'First name')
+    dob = payroll._get_value(i, 'Date of birth')
+    df = emis[(emis['teacher_surname'] == surname)&(emis['teacher_name'] == firstname)&(emis['Date of birth'] == dob)]
+    for e_id in df['Numéro EMIS'].unique():
+      res.append((p_id, e_id))
+      j = df[df['Numéro EMIS'] == e_id].index
+      try: df_emis = df_emis.drop(index = j, inplace = False)
+      except: pass
+      try: df_payroll = df_payroll.drop(index = i, inplace = False)
+      except: pass
+  return res, df_emis, df_payroll
 
 
 def step_1_and_2(emis, payroll):
@@ -63,28 +63,28 @@ def step_1_and_2(emis, payroll):
   p_dict = {}
 
   for elem in mapping:
-      e, p = elem
-      if e not in e_dict.keys():
-          e_dict[e] = [p]
-      elif p not in e_dict[e]:
-          e_dict[e].append(p)
+    e, p = elem
+    if e not in e_dict.keys():
+      e_dict[e] = [p]
+    elif p not in e_dict[e]:
+      e_dict[e].append(p)
 
-      if p not in p_dict.keys():
-          p_dict[p] = [e]
-      elif p not in p_dict[p]:
-          p_dict[p].append(e)
+    if p not in p_dict.keys():
+      p_dict[p] = [e]
+    elif p not in p_dict[p]:
+      p_dict[p].append(e)
 
   unique_mapping = []
   multiple_mapping = []
   for e in e_dict.keys():
-      if len(e_dict[e]) == 1:
-          p = e_dict[e][0]
-          if p_dict[p] == [e]: unique_mapping.append((e, p))
-      else:
-          for p in e_dict[e]:
-              if (e,p) not in multiple_mapping: multiple_mapping.append((e, p))
-              for e_p in p_dict[p]:
-                  if (e_p, p) not in multiple_mapping: multiple_mapping.append((e_p, p))
+    if len(e_dict[e]) == 1:
+      p = e_dict[e][0]
+      if p_dict[p] == [e]: unique_mapping.append((e, p))
+    else:
+      for p in e_dict[e]:
+        if (e,p) not in multiple_mapping: multiple_mapping.append((e, p))
+        for e_p in p_dict[p]:
+            if (e_p, p) not in multiple_mapping: multiple_mapping.append((e_p, p))
 
 
   return unique_mapping, multiple_mapping
