c73810f2eeafdcd8603cdc6787c433227031e2f0
Romain Fouilland <romain.fd@gmail.com>
2021-01-31 12:36:18 +0100

    Collecting ghosts

--- step4/handler.py
+++ step4/handler.py
@@ -1,4 +1,21 @@
-def start(event, context):
-    print(event)
+from utils import *
 
-    return event
+def start(event, context):
+  emis, payroll = read_data_from_event(event)
+
+  # The remaining teachers in payroll are ghosts
+  ghosts = list(payroll['Numéro Solde'])
+
+  response = {
+    "source_teachers": event["source_teachers"],
+    "source_payrolls": event["source_payrolls"],
+    "payroll_duplicates": event["payroll_duplicates"],
+    "step1_ids": event["step1_ids"],
+    "step2_ids": event["step2_ids"],
+    "step3_ids": event["step3_ids"],
+    "step4_ids": event["step4_ids"],
+    "ghosts": json.dumps(ghosts, cls=NpEncoder)
+  }
+
+  print(response)
+  return response
